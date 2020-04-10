5dac088b32bcb56df5fa50bbfedcba5029a63294
sammiefrog <sammiefrog@live.com>
2020-04-10 14:58:54 -0500

    added three specific questions

--- app.js
+++ app.js
@@ -4,19 +4,15 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function inquireQ() {
-    inquirer
-  .prompt([
-    {
+const questions = [{
       type: "input",
       message: "What is your full name?",
       name: "fullName"
@@ -38,32 +34,64 @@ function inquireQ() {
         name: "role"
     },
     {
-        type: "checkbox",
+        type: "list",
         message: "Number of Employees:",
-        choices: ["1", "2", "3", "No more Employees"],
+        choices: [1, 2, 3, "No more Employees"],
         name: "numOfEmps"
     }
-  ])
-  .then(function(response) {
-    console.log(response);
-  });
+];
 
+
+function inquireQ(){
+    inquirer
+    .prompt(questions)
+    .then(function(response) {
+        if(response.role === "Manager"){
+            inquirer.prompt({
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNum"
+            }).then(function(managerOffice){
+                console.log(managerOffice);
+            })
+        }
+        else if(response.role === "Engineer"){
+            inquirer.prompt({
+            type: "input",
+            message: "What is your github user name??",
+            name: "github"
+            }).then(function(engineerGH){
+                console.log(engineerGH);
+            })
+        }
+        else if(response.role === "Intern"){
+            inquirer.prompt({
+            type: "input",
+            message: "What school did you attend?",
+            name: "school"
+            }).then(function(internSchool){
+                console.log(internSchool);
+            })
+        }
+    });
 }
-​
+
+inquireQ();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
