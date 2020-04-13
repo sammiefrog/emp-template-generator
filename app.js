f1a523879673448e93d84af09001bbf2552db06a
sammiefrog <sammiefrog@live.com>
2020-04-13 10:43:53 -0500

    making html pages

--- app.js
+++ app.js
@@ -102,11 +102,11 @@ function inquireQ(){
     break;
         case "Finish team":
             if (team.length > 0){
-                render(team);
-                console.log("all done!")
+                writeHTML(render(team));
+                console.log("All done!")
             }
             else{
-                console.log("no team members");
+                console.log("There's no Team Members!");
                 inquireQ();
             }
         break;
@@ -118,38 +118,12 @@ function inquireQ(){
 });
 }
 
-
 inquireQ();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-function writeHTML(render){
-    fs.writeFile(outputPath, template, function (err) {
-  
+function writeHTML(HTML){
+    fs.writeFileSync(outputPath, HTML, function (err) {
         if (err) {
             return console.log(err);
-        }
-        console.log("Success!");
-  
+        }  
     });
 }
-console.log(writeHTML);
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
